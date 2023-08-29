c337eec6d95bf24a4dce72752618bfa8a2de3d31
pexApred <120075705+pexApred@users.noreply.github.com>
2023-08-29 15:07:08 -0400

    fixed mongodb connection in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wwofdrslide");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/wwofdrslide");
 
 module.exports = mongoose.connection;
