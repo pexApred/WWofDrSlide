8b36e0a65dd2bfccd9acc5c211bd92451a7b7626
pexApred <120075705+pexApred@users.noreply.github.com>
2023-08-29 17:09:44 -0400

    fixed file paths in server.js

--- server/server.js
+++ server/server.js
@@ -30,15 +30,15 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client_wwofdrslide/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client_wwofdrslide/build/index.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client_wwofdrslide/build/index.html'));
 });
 
 // app.use(routes);
