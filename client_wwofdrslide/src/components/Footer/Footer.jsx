2d4e8922c0865cad7072580b67acb732c011376e
pexApred <120075705+pexApred@users.noreply.github.com>
2023-12-08 11:03:54 -0500

    Adjusted footer.jsx and footer.css to better display content

--- client_wwofdrslide/src/components/Footer/Footer.jsx
+++ client_wwofdrslide/src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer id='footer-container' className='mt-auto'>
-      <Container>
+    <>
+      <Container fluid id='footer-container' className='mt-auto'>
         <Row className='footer'>
           <Col xs={12} md={4} className='text-center text-md-left md-0'>
             <p className='mb-0 cr'>Author: Michael H. Schecter, MD</p>
@@ -21,7 +21,7 @@ const Footer = () => {
           </Col>
         </Row>
       </Container>
-    </footer>
+    </>
   );
 };
 
