fdde9c730f904fdb15d7a3e62122e574ca6777de
pexApred <120075705+pexApred@users.noreply.github.com>
2024-01-01 14:16:09 -0500

    testing footer

--- client_wwofdrslide/src/components/Footer/Footer.jsx
+++ client_wwofdrslide/src/components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 import './Footer.css'
 
 const Footer = () => {
   return (
     <>
       <footer className="footer-container mt-auto">
-          <Row className="footer">
+          <div className="footer">
             {/* <Col xs={12} md={4} className="text-center text-md-left md-0">
               <p className="mb-0 cr">Author: Michael H. Schecter, MD</p>
             </Col> */}
-            <Col className="text-center text-md-right">
+            <div className="text-center text-md-right">
               <p className="mb-0 cr">© 2023 <span className='text-sz'>T</span>he <span className='text-sz'>W</span>onderful <span className='text-sz'>W</span>orld of d<span className='text-sz'>R</span> slide</p>
-            </Col>
+            </div>
             {/* <Col xs={12} md={4} className="text-center md-0 ">
               <p className="mb-0 cr">
                 <a href="https://emmanuellakis.dev">
@@ -20,7 +20,7 @@ const Footer = () => {
                 </a>
               </p>
             </Col> */}
-          </Row>
+          </div>
       </footer>
     </>
   );
