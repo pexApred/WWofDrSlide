492fbbb4d9a544b5f325d9a56422e765909078b3
pexApred <120075705+pexApred@users.noreply.github.com>
2024-01-30 00:28:11 -0500

    Banner sizing improved

--- client_wwofdrslide/src/components/Banner/Banner.jsx
+++ client_wwofdrslide/src/components/Banner/Banner.jsx
@@ -5,27 +5,18 @@ const Banner = () => {
   return (
     <div className="banner">
       <p className="banner-text">
-        Purchase a{" "}
+        Want to unlock the riddles? Purchase the book {' '}
         <a
-          className="purchase-link-hard"
+          className="purchase-link"
           href="https://www.warrenpublishing.net/store/p502/PRE-ORDER_The_Wonderful_World_of_dR_slide%3A_A_Book_of_Riddles_for_All_%28hard_cover%29_by_Michael_Schecter%2C_MD.html"
-        >
-          hard cover
-        </a>{" "}
-        or{"  "}
-        <a
-          className="purchase-link-soft"
-          href="https://www.warrenpublishing.net/store/p503/PRE-ORDER_The_Wonderful_World_of_dR_slide%3A_A_Book_of_Riddles_for_All_%28soft_cover%29_by_Michael_Schecter%2C_MD.html"
-        >
-          soft cover
-        </a>{" "}
-        of <br />
-        <span className="banner-title">
+        > here
+        {/* <span className="banner-title">
           T<span className="text-b">HE </span>W
           <span className="text-b">ONDERFUL </span>W
           <span className="text-b">ORLD OF D</span>R
           <span className="text-b"> SLIDE: A BOOK OF RIDDLES FOR ALL</span>
-        </span>
+        </span> */}
+        </a>
       </p>
     </div>
   );
