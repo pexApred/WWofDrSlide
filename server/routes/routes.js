633f7984c69cd7c85533c66b3c75d6b43092dbb5
pexApred <120075705+pexApred@users.noreply.github.com>
2024-01-31 13:48:51 -0500

    Reverted back to using production url only until bigger update needed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const UserModel = require('../models/User');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
+// const UserModel = require('../models/User');
+// const jwt = require('jsonwebtoken');
+// const config = require('../config/config');
 const router = express.Router();
 
-const isProduction = process.env.NODE_ENV === 'production';
-const backendBaseURL = isProduction
-? 'https://www.thewonderfulworldofdrslide.com'
-: 'http://localhost:5173';
-const frontendBaseURL = isProduction
-? 'https://www.thewonderfulworldofdrslide.com'
-: 'http://localhost:3000'
+// const isProduction = process.env.NODE_ENV === 'production';
+// const backendBaseURL = isProduction
+// ? 'https://www.thewonderfulworldofdrslide.com'
+// : 'http://localhost:5173';
+// const frontendBaseURL = isProduction
+// ? 'https://www.thewonderfulworldofdrslide.com'
+// : 'http://localhost:3000'
 
 
-router.post('/forgot-password', async (req, res) => {
-    const { email } = req.body;
-    const user = await UserModel.findOne({ email });
-    if (!user) {
-        return res.status(404).send({ message: "User not found" });
-    }
+// router.post('/forgot-password', async (req, res) => {
+//     const { email } = req.body;
+//     const user = await UserModel.findOne({ email });
+//     if (!user) {
+//         return res.status(404).send({ message: "User not found" });
+//     }
 
     // const token = jwt.sign({ id: user._id }, config.JWT_SECRET, { expiresIn: "1d" });
     // const resetLink = `https://www.thewonderfulworldofdrslide.com/reset_password/${user._id}/${token} || http://localhost:5173/reset_password/${user._id}/${token}`;
 
-    res.send({ message: "Password reset email sent." });
-});
+//     res.send({ message: "Password reset email sent." });
+// });
 
 router.get('/reset-password', (req, res) => {
     const { token } = req.query;
-    const frontendResetPasswordUrl = `${frontendBaseURL}/reset-password?token=${token}`;
+    const frontendResetPasswordUrl = `https://www.thewonderfulworldofdrslide.com/reset-password?token=${token}`;
     res.redirect(frontendResetPasswordUrl);
 });
 
