b785bb4980b9e6b9e011ded05b70e4019557fd45
pexApred <120075705+pexApred@users.noreply.github.com>
2024-01-31 14:08:44 -0500

    Commented out redirection loop to solve issue of forgotPassword

--- server/server.js
+++ server/server.js
@@ -42,12 +42,12 @@ app.use(express.json());
 
 app.use(authMiddleware);
 
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect('https://' + req.headers.host + req.url);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+//     return res.redirect('https://' + req.headers.host + req.url);
+//   }
+//   next();
+// });
 
 app.use(routes);
 
